cbe8af882cbf22684a94bc79cdf08459c56e8569
Joseph James Smyth <jjsmyth1984@gmail.com>
2024-11-09 14:54:21 +0000

    Latest updates.

--- src/Model/Mailer.php
+++ src/Model/Mailer.php
@@ -2,6 +2,7 @@
 
 namespace App\Model;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,23 +13,27 @@ class Mailer extends AbstractController
 
     /**
      * @param MailerInterface $mailerInterface
-     * @return void
+     * @return Response
      * @throws TransportExceptionInterface
      */
-    public function sendEmail(MailerInterface $mailerInterface): void
+    public function sendEmail(MailerInterface $mailerInterface): Response
     {
+
+        // Email builder
         $email = (new Email())
             ->from('noreply@jjsmyth.net')
             ->to('joe@jjsmyth.net')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
             ->subject('Time for Symfony Mailer!')
             ->text('Sending emails is fun again!')
             ->html('<p>See Twig integration for better HTML integration!</p>');
 
+
+        // Send email
         $mailerInterface->send($email);
+
+        // Return response
+        return new Response('Email was sent successfully.');
+
     }
 
 }
