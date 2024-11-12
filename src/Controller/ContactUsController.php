c1b39084cdce280e14003af4fb31afb7bef210d1
Joseph James Smyth <jjsmyth1984@gmail.com>
2024-11-12 11:31:18 +0000

    Test installation of code sniffer

--- src/Controller/ContactUsController.php
+++ src/Controller/ContactUsController.php
@@ -19,8 +19,13 @@ class ContactUsController extends AbstractController
      * @throws TransportExceptionInterface
      */
     #[Route('/contact-us', name: 'app_contact_us', methods: ['POST'])]
-    public function contactUs(Request $request, ContactUsForm $contactUsForm, MailerInterface $mailerInterface, Mailer $mailer, EntityManagerInterface $entityManager): JsonResponse
-    {
+    public function contactUs(
+        Request $request,
+        ContactUsForm $contactUsForm,
+        MailerInterface $mailerInterface,
+        Mailer $mailer,
+        EntityManagerInterface $entityManager,
+    ): JsonResponse {
         // Get the posted data
         $postData = (array) json_decode($request->getContent(), true);
 
@@ -31,7 +36,12 @@ class ContactUsController extends AbstractController
 
         // Honeypot should not be passed from the frontend and should also not have a value
         if (isset($postData['contact_us_honey_pot']) && !$postData['contact_us_honey_pot']) {
-            return new JsonResponse(['success' => false, 'message' => 'Honeypot isset and has a value which is incorrect']);
+            return new JsonResponse(
+                [
+                    'success' => false,
+                    'message' => 'Honeypot isset and has a value which is incorrect'
+                ]
+            );
         }
 
         // Instantiate contact us form
