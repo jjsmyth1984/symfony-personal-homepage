5c28c6cf13d88c6158a43757d3096110cfa5f1d4
Joseph James Smyth <jjsmyth1984@gmail.com>
2024-11-19 17:33:28 +0000

    Adding new registration form for homepage user account along with registration form.

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/registration', name: 'app_registration')]
+    public function index(UserPasswordHasherInterface $passwordHasher): Response
+    {
+
+        $user = new User();
+        $plaintextPassword = '';
+
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+
+        return $this->render('registration/index.html.twig', [
+            'controller_name' => 'RegistrationController',
+        ]);
+    }
+}
